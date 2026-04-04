839c42c6f9551870d08f8a334c27a3e0bd16db4d
alimost <alimost1@gmail.com>
2026-04-04 16:20:46 +0100

    force deploy: language switcher move and flags

--- wp-content/themes/mashaussure/header.php
+++ wp-content/themes/mashaussure/header.php
@@ -72,6 +72,13 @@ else: ?>
 					<a href="#" class="masha-header-link masha-wishlist masha-desktop-only"><svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"/></svg> <?php esc_html_e('Wishlist', 'mashaussure'); ?></a>
 				<?php
 endif; ?>
+				<div class="masha-mobile-langs">
+					<ul class="masha-lang-list">
+						<?php if (function_exists('xili_language_list')) {
+							xili_language_list('<li>', '</li>', 'navmenu', true, false);
+						} ?>
+					</ul>
+				</div>
 				<?php if (class_exists('WooCommerce')): ?>
 				<a href="<?php echo esc_url(wc_get_cart_url()); ?>" class="elena-cart-btn masha-cart" title="<?php esc_attr_e('Cart', 'mashaussure'); ?>">
 					<svg width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M6 2L3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4z"/><line x1="3" y1="6" x2="21" y2="6"/><path d="M16 10a4 4 0 0 1-8 0"/></svg>
